47516a7f010d1e1a3de8346fc36c65f2b66895f4
Profeleo14 <jlpinedau7@gmail.com>
2025-05-08 07:53:21 -0500

    Fix: Se arregla código de Usuarios

--- 3. Tercer Trimestre/Bases de datos/Creacion Usuarios MYSQL.sql
+++ 3. Tercer Trimestre/Bases de datos/Creacion Usuarios MYSQL.sql
@@ -3,9 +3,9 @@
 CREATE USER 'nombre_usuario'@'localhost' IDENTIFIED BY 'tu_contrasena';
 
 -- Ejemplo: 
-CREATE USER 'leo'@'localhost' IDENTIFIED BY '12345';
+CREATE USER 'leo'@'localhost' IDENTIFIED BY '123';
 CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin123';
-CREATE USER 'dba'@'localhost'IDENTIFIED BY 'dba123';
+CREATE USER 'dba'@'localhost'IDENTIFIED BY 'dba12345';
 -- 2. Proporcionar el acceso requerido al usuario con la información que requiere.
 
 GRANT ALL PRIVILEGES ON * . * TO 'nombre_usuario'@'localhost'; 
@@ -15,8 +15,6 @@ GRANT ALL PRIVILEGES ON * . * TO 'leo'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'dba'@'localhost';
 
--- 3. Una vez otorgado los servicios, hay que asegurarse siempre de refrescar todos los privilegios.
-FLUSH PRIVILEGES; 
 
 -- 4. Como se vio en sesiones anteriores ingresamos al motor con el usuario nuevo por medio de la sentencia:
 Ejemplo: 
